01443c4195f42b39a197192acb4ab79c8e216dd6
Mercedes Quinterno <79026648+Mercedes-Quinterno@users.noreply.github.com>
2023-02-27 23:33:59 -0300

    Reportes, Balances, Categorías

--- app.js
+++ app.js
@@ -157,3 +157,159 @@ const cargarDatosOperacion = (operacion) => {
 	$("#editar-categoria-operacion").value = operacion.Categoria;
 	$("#editar-fecha-operacion").valueAsDate = new Date(operacion.Fecha);
 }
+
+/* Categorias */
+
+const actualizarCategorias = () => {
+    actualizarSelectoresCategorias()
+    actualizarListaCategorias()
+  }
+  
+  const actualizarSelectoresCategorias = () => {
+      const selects = $$('.categorias-select')
+  
+      selects.forEach((select) => {
+          const esFiltro = select.classList.contains('filtro-categoria');
+          select.innerHTML = esFiltro ? '<option value="todas">Todas</option>' : ''
+          const categorias = obtenerCategorias();
+          categorias.forEach((categoria) => {
+              select.innerHTML += `<option value="${categoria.Id}">${categoria.Nombre}</option>`
+          })
+      })
+  }
+  
+  const actualizarListaCategorias = () => {
+      const tabla = $('#tabla-categorias');
+      tabla.innerHTML = '';
+  
+      const categorias = obtenerCategorias();
+      categorias.forEach((categoria) => {
+          const itemCategoria = document.createElement('tr')
+          itemCategoria.innerHTML += `
+            <th>${categoria.Nombre}</th>
+            <td><div class="field is-grouped">
+                <p class="control"><button class="editar-categoria button is-info">Editar</button></p>
+                <p class="control"><button class="eliminar-categoria button is-danger">Eliminar</button></p>
+              </div></td> 
+          `
+          
+          const editarAccion = itemCategoria.querySelector('.editar-categoria')
+          const eliminarAccion = itemCategoria.querySelector('.eliminar-categoria')
+  
+          editarAccion.onclick = () => {
+            cargarDatosCategoria(categoria)
+            mostrarSeccion('editar-categoria')
+          }
+  
+          eliminarAccion.onclick = () => {
+              const categorias = eliminarCategoria(categoria.Id, obtenerCategorias());
+              const operaciones = eliminarOperacionesPorCategoria(categoria.Id, obtenerOperaciones());
+              actualizarDatos({categorias, operaciones});
+          }
+          
+          tabla.append(itemCategoria);
+      })
+  }
+  
+  const cargarDatosCategoria = (categoria) => {
+      const nombreCategoria = $("#editar-nombre-categoria")
+      nombreCategoria.value = categoria.Nombre;
+      nombreCategoria.setAttribute("categoria", categoria.Id);
+  }
+  
+  /* Balance */
+  
+  const actualizarBalance = (operaciones = obtenerOperaciones()) => {
+    const { ganancias, gastos, balance } = obtenerBalance(operaciones)
+    $("#ganancias").innerHTML = `+$${Math.abs(ganancias)}`
+    $("#gastos").innerHTML = `-$${Math.abs(gastos)}`
+  
+    $("#balance").classList.remove("has-text-danger", "has-text-success")
+    let operador = ""
+  
+    if (balance > 0) {
+      $('#balance').classList.add("has-text-success")
+      operador = '+'
+    } else if (balance < 0) {
+      $("#balance").classList.add("has-text-danger")
+      operador = "-"
+    }
+  
+    $("#balance").innerHTML = `${operador}$${Math.abs(balance)}`
+  }
+  
+  /* Reportes */
+  
+  const actualizarReportes = () => {
+      const operaciones = obtenerOperaciones();
+      const categorias = obtenerCategorias();
+      const suficientesOperaciones = operaciones.length > 1;
+      if (suficientesOperaciones) {
+          $("#con-reportes").classList.remove("is-hidden");
+          $("#sin-reportes").classList.add("is-hidden");
+      } else {
+          $("#sin-reportes").classList.remove("is-hidden");
+          $("#con-reportes").classList.add("is-hidden");
+      }
+      
+      const {
+          categoriaTopGanancias,
+          categoriaTopGastos,
+          categoriaTopBalance,
+      } = obtenerResumen(operaciones, categorias);
+      
+      $("#categoria-top-ganancias-nombre").innerHTML = categoriaTopGanancias.categoria.Nombre;
+      $("#categoria-top-ganancias-monto").innerHTML = formatearMonto(categoriaTopGanancias.balance.ganancias);
+      $("#categoria-top-ganancias-monto").classList.add(colorParaMonto(categoriaTopGanancias.balance.ganancias));
+  
+      $("#categoria-top-gastos-nombre").innerHTML = categoriaTopGastos.categoria.Nombre;
+      $("#categoria-top-gastos-monto").innerHTML = formatearMonto(categoriaTopGastos.balance.gastos * -1);
+      $("#categoria-top-gastos-monto").classList.add(colorParaMonto(categoriaTopGastos.balance.gastos * -1));
+      
+      $("#categoria-top-balance-nombre").innerHTML = categoriaTopBalance.categoria.Nombre;
+      $("#categoria-top-balance-monto").innerHTML = formatearMonto(categoriaTopBalance.balance.balance);
+      $("#categoria-top-balance-monto").classList.add(colorParaMonto(categoriaTopBalance.balance.balance));
+      
+      const totalesPorCategorias = obetenerTotales(operaciones, categorias);
+      
+      const tabla = $("#tabla-totales-por-categorias");
+      totalesPorCategorias.forEach((totalesPorCategoria) => {
+          const itemTotal = document.createElement('tr')
+  
+          const textoGanancias = formatearMonto(totalesPorCategoria.balance.ganancias);
+          const textoGastos = formatearMonto(totalesPorCategoria.balance.gastos * -1);
+          const textoBalance = formatearMonto(totalesPorCategoria.balance.balance);
+          const colorGanancias = colorParaMonto(totalesPorCategoria.balance.ganancias);
+          const colorGastos = colorParaMonto(totalesPorCategoria.balance.gastos * -1);
+          const colorBalance = colorParaMonto(totalesPorCategoria.balance.balance);
+          
+          itemTotal.innerHTML += `
+            <th>${totalesPorCategoria.categoria.Nombre}</th>
+            <td class="${colorGanancias}">${textoGanancias}</td>
+            <td class="${colorGastos}">${textoGastos}</td>
+            <td class="${colorBalance}">${textoBalance}</td>
+          `
+          
+          tabla.append(itemTotal);
+      });
+  }
+  
+  const formatearMonto = (monto) => {
+      if (monto > 0) {
+          return `+${monto}`;
+      } else {
+          return monto;
+      }
+  }
+  
+  const colorParaMonto = (monto) => {
+      if (monto > 0) {
+          return "has-text-success";
+      } else if (monto === 0) {
+          return "has-text-black";
+      } else {
+          return "has-text-danger";
+      }
+  }
+  
+  
